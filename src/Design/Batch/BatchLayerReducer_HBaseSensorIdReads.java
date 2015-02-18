18f5e53792d4585cd3a6dfe46bbf154eca9bc58b
Braden Lunn <braden.lunn@gmail.com>
2015-02-18 17:46:05 -0500

    Added a service for creating HBase tables and adding data

--- src/Design/Batch/BatchLayerReducer_HBaseSensorIdReads.java
+++ src/Design/Batch/BatchLayerReducer_HBaseSensorIdReads.java
@@ -1,21 +1,24 @@
 package Design.Batch;
 import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
 public class BatchLayerReducer_HBaseSensorIdReads
-extends Reducer<Text, IntWritable, Text, IntWritable> {
+extends Reducer<Text, Text, Text, Text> {
 	@Override
-	public void reduce(Text key, Iterable<IntWritable> values,
+	public void reduce(Text key, Iterable<Text> values,
 			Context context)
 					throws IOException, InterruptedException {
 
-		//we have a key and bunch of temperature values
-		int maxValue = Integer.MIN_VALUE;
-		for (IntWritable value : values) {
-			maxValue = Math.max(maxValue, value.get());
+		//create table
+		HBaseService.CreateTable("SensorView");
+
+		//iterate through values and write to table
+		for (Text value : values) {
+			
 		}
 		//outputs the key which is year and the max value
-		context.write(key, new IntWritable(maxValue));
+		context.write(key, new Text());
 	}
 }
---
+++ src/Design/Batch/HBaseService.java
@@ -0,0 +1,101 @@
+package Design.Batch;
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.exceptions.MasterNotRunningException;
+import org.apache.hadoop.hbase.exceptions.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.util.Bytes;
+
+
+public class HBaseService {
+	
+	public static boolean CreateTable(String tableName){
+		try {
+			// Instantiating configuration class
+			Configuration conf = HBaseConfiguration.create();
+			
+			// Instantiating HBaseAdmin class
+			HBaseAdmin admin = new HBaseAdmin(conf);
+			
+			// Verifying the table doesn't exist
+			boolean tableExists = admin.tableExists(tableName);
+
+			if(tableExists)
+			{
+				admin.close();
+				return true;
+			}
+			else
+			{
+					// Instantiating table descriptor class
+					HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
+
+					// Adding column families to table descriptor
+					//To optimize performance, keeping names as small as possible
+					tableDescriptor.addFamily(new HColumnDescriptor("d"));
+
+					// Execute the table through admin
+					admin.createTable(tableDescriptor);
+			}
+			admin.close();
+			
+		} catch (MasterNotRunningException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		catch (ZooKeeperConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean AddData(String tableName, String rowKey, String columnFamily, String columnQualifier, String value)
+	{
+		try {
+			// Instantiating Configuration class
+			  Configuration config = HBaseConfiguration.create();
+
+			  // Instantiating HTable class
+			  HTable hTable = new HTable(config, tableName);
+
+			  // Instantiating Put class
+			  // accepts a row name.
+			  Put p = new Put(Bytes.toBytes(rowKey)); 
+
+			  // adding values using add() method
+			  // accepts column family name, qualifier/row name ,value
+			  p.add(Bytes.toBytes(columnFamily),
+			  Bytes.toBytes(columnQualifier),Bytes.toBytes(value));
+			  
+			  // Saving the put Instance to the HTable.
+			  hTable.put(p);
+			  System.out.println("data inserted");
+			  
+			  // closing HTable
+			  hTable.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		
+	      return true;
+	} 
+}
