37281c111395c988be94d5a2cba66582eaa1e952
Braden Lunn <braden.lunn@gmail.com>
2015-02-18 18:37:57 -0500

    added the reducer class to write to HBase table

--- src/Design/Batch/BatchLayerReducer_HBaseSensorIdReads.java
+++ src/Design/Batch/BatchLayerReducer_HBaseSensorIdReads.java
@@ -12,11 +12,33 @@ extends Reducer<Text, Text, Text, Text> {
 					throws IOException, InterruptedException {
 
 		//create table
-		HBaseService.CreateTable("SensorView");
+		String tableName = "SensorView";
+		HBaseService.CreateTable(tableName);
 
 		//iterate through values and write to table
 		for (Text value : values) {
+			//Value format: Timestamp:Value. Value is sensor reading
+			String [] valueArray = value.toString().split(":");
 			
+			//rowKey is sensorId plus timestamp. 
+			//Ensures that records are sorted by timestamp and are next to each other
+			String rowKey = key.toString() + "-" + valueArray[1];
+			
+			//column family static from HBaseService.CreateTable
+			String columnFamily = "d";
+			
+			//dynamically created on data input. set to optimize memory so saving keystrokes on name.
+			String columnQualifier = "val";
+			
+			//cell value is the reading from the sensor
+			String sensorReading = valueArray[1];
+			
+			boolean dataAdded = HBaseService.AddData(tableName, rowKey, columnFamily, columnQualifier, sensorReading);
+			
+			if(!dataAdded)
+				System.out.println("Data didn't get added");
+			else
+				System.out.println("Data outputted to HBase table: " + tableName);
 		}
 		//outputs the key which is year and the max value
 		context.write(key, new Text());
